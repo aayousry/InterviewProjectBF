22b61214942f267a8a6f03449f20812d69462b0d
Abdelrahman Hassan <aayousry@gmail.com>
2022-03-17 18:58:01 -0400

    added get quote api call implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {GetAllowedCurrenciesService} from 'src/app/services/get-allowed-currencies.service';
 import {GetDestinationCountryService} from 'src/app/services/get-destination-country.service';
 import {GetHostCountryService} from 'src/app/services/get-host-country.service';
+import {GetQuoteService} from 'src/app/services/get-quote.service';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +14,19 @@ export class AppComponent implements OnInit{
   resultCurrencies: Object = "null";
   resultDestinationCountries: Object = "null";
   resultHostCountries: Object = "null";
+  resultQuote: Object = "null";
 
-  constructor(public getAllowedCurrenciesService: GetAllowedCurrenciesService,
+  constructor(
+  public getQuoteService: GetQuoteService,
+  public getAllowedCurrenciesService: GetAllowedCurrenciesService,
   public getDestinationCountryService: GetDestinationCountryService,
   public getHostCountryService: GetHostCountryService,) { }
 
   ngOnInit(): void {
     this.getAllowedCurrencies();
-    this.getDestinationCountry();
-    this.getHostCountry();
+//     this.getDestinationCountry();
+//     this.getHostCountry();
+    this.getQuote();
   }
 
   getAllowedCurrencies(){
@@ -43,13 +48,22 @@ export class AppComponent implements OnInit{
   }
 
   getHostCountry(){
-      this.getHostCountryService.getHostCountry().subscribe(countriesResult => {
-          this.resultHostCountries = countriesResult;
-          console.log("!!!! Here:   " + JSON.stringify(this.resultHostCountries));
-      }, error => {
-        console.log("ERORRRRRR" + JSON.stringify(error));
-      });
-    }
+    this.getHostCountryService.getHostCountry().subscribe(countriesResult => {
+        this.resultHostCountries = countriesResult;
+        console.log("!!!! Here:   " + JSON.stringify(this.resultHostCountries));
+    }, error => {
+      console.log("ERORRRRRR" + JSON.stringify(error));
+    });
+  }
+
+  getQuote(){
+    this.getQuoteService.getQuote().subscribe(quoteResult => {
+        this.resultQuote = quoteResult;
+        console.log("!!!! Here:   " + JSON.stringify(this.resultQuote));
+    }, error => {
+      console.log("ERORRRRRR" + JSON.stringify(error));
+    });
+  }
 
 
 }
--- src/app/services/get-quote.service.ts
+++ src/app/services/get-quote.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetQuoteService {
 
-  constructor() { }
+  private baseUrl = "https://coding-challenge-api.bfdevsite.com/api/v1/token/quotation";
+
+  private headers= new HttpHeaders()
+    .set('content-type', 'application/json')
+    .set('Authorization', 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUYW5naWVycyBJbnRlcm5hdGlvbmFsIiwiaWF0IjoxNjI4MDA4MjAxLCJleHAiOjIyNTkxNjAyMDEsIm5iZiI6MTYyODAwODIwMSwianRpIjoiVkZzU3I5TE9hbHNKN1d0QyIsImFmZmlsaWF0ZV9pZCI6MSwicHJvZHVjdF9pZHMiOnsiMSI6MiwiMiI6NiwiMyI6NywiNCI6OCwiNSI6OSwiNiI6MTAsIjciOjExLCI4IjoxMn0sInBlcm1pc3Npb25zIjpbIlFRQyIsIkdBQyIsIkdEQyIsIkdIQyIsIkdSVSJdLCJzdWIiOiJQYXJ0bmVyIEFjY2VzcyBUb2tlbiJ9.sTZsa-ufE4MkDaqKMz380HmGZjc2evxOXw9M1H1M_Xs');
+
+  private params = {
+     "product_id": 8,
+     "age": '28,16',
+     "currency_id": 'USD',
+     "destination_country_ids": 'CZ',
+     "host_country_id": 'US',
+     "country_state": 'AL',
+     "start_date": '2022-10-01',
+     "end_date": '2022-10-30',
+     "trip_cost": 500,
+     "deposit_date": '2022-10-01',
+     "winter_sport_extension": true
+  }
+
+  constructor(private http: HttpClient) {
+  }
+
+  getQuote(){
+    return this.http.post(`${this.baseUrl}`, this.params, { 'headers': this.headers });
+  }
+
 }
+
