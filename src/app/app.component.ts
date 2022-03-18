80a233d525713c1b0e5527f9a298292275de72a2
Abdelrahman Hassan <aayousry@gmail.com>
2022-03-18 14:22:50 -0400

    3 out of 4 api calls in the right time and place. Some changes to form to make more sense and be dependent on user selecting host country.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {GetAllowedCurrenciesService} from 'src/app/services/get-allowed-currencies.service';
-import {GetDestinationCountryService} from 'src/app/services/get-destination-country.service';
-import {GetHostCountryService} from 'src/app/services/get-host-country.service';
-import {GetQuoteService} from 'src/app/services/get-quote.service';
 
 @Component({
   selector: 'app-root',
@@ -11,59 +7,10 @@ import {GetQuoteService} from 'src/app/services/get-quote.service';
 })
 export class AppComponent implements OnInit{
   title = 'A-Hassan-BattleFace-App';
-  resultCurrencies: Object = "null";
-  resultDestinationCountries: Object = "null";
-  resultHostCountries: Object = "null";
-  resultQuote: Object = "null";
 
-  constructor(
-  public getQuoteService: GetQuoteService,
-  public getAllowedCurrenciesService: GetAllowedCurrenciesService,
-  public getDestinationCountryService: GetDestinationCountryService,
-  public getHostCountryService: GetHostCountryService,) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.getAllowedCurrencies();
-//     this.getDestinationCountry();
-//     this.getHostCountry();
-    this.getQuote();
   }
-
-  getAllowedCurrencies(){
-    this.getAllowedCurrenciesService.getAllowedCurrencies().subscribe(currenciesResult => {
-        this.resultCurrencies = currenciesResult;
-        console.log("!!!! Here:   " + JSON.stringify(this.resultCurrencies));
-    }, error => {
-      console.log("ERORRRRRR" + JSON.stringify(error));
-    });
-  }
-
-  getDestinationCountry(){
-    this.getDestinationCountryService.getDestinationCountry().subscribe(countriesResult => {
-        this.resultDestinationCountries = countriesResult;
-        console.log("!!!! Here:   " + JSON.stringify(this.resultDestinationCountries));
-    }, error => {
-      console.log("ERORRRRRR" + JSON.stringify(error));
-    });
-  }
-
-  getHostCountry(){
-    this.getHostCountryService.getHostCountry().subscribe(countriesResult => {
-        this.resultHostCountries = countriesResult;
-        console.log("!!!! Here:   " + JSON.stringify(this.resultHostCountries));
-    }, error => {
-      console.log("ERORRRRRR" + JSON.stringify(error));
-    });
-  }
-
-  getQuote(){
-    this.getQuoteService.getQuote().subscribe(quoteResult => {
-        this.resultQuote = quoteResult;
-        console.log("!!!! Here:   " + JSON.stringify(this.resultQuote));
-    }, error => {
-      console.log("ERORRRRRR" + JSON.stringify(error));
-    });
-  }
-
 
 }
--- src/app/views/get-quote-form-component/get-quote-form-component.component.ts
+++ src/app/views/get-quote-form-component/get-quote-form-component.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {GetAllowedCurrenciesService} from 'src/app/services/get-allowed-currencies.service';
+import {GetDestinationCountryService} from 'src/app/services/get-destination-country.service';
+import {GetHostCountryService} from 'src/app/services/get-host-country.service';
+import {GetQuoteService} from 'src/app/services/get-quote.service';
 
 @Component({
   selector: 'app-get-quote-form-component',
@@ -8,12 +12,14 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class GetQuoteFormComponentComponent implements OnInit {
 
+  hostCountrySelectedBool: boolean  = false;
+
   getQuoteForm = new FormGroup({
     hostCountry: new FormControl('', Validators.required),
-    hostState: new FormControl(''),
+    hostState: new FormControl({value:'', disabled: !this.hostCountrySelectedBool}),
     destinationCountry: new FormControl('', Validators.required),
     age: new FormControl('', Validators.required),
-    currency: new FormControl('', Validators.required),
+    currency: new FormControl({value:'', disabled: !this.hostCountrySelectedBool}, Validators.required),
     totalCost: new FormControl('', Validators.required),
     startDate: new FormControl('', Validators.required),
     endDate: new FormControl('', Validators.required),
@@ -21,9 +27,67 @@ export class GetQuoteFormComponentComponent implements OnInit {
     winterSport: new FormControl('', Validators.required),
   });
 
-  constructor() { }
+  resultCurrencies;
+  resultDestinationCountries;
+  resultHostCountries;
+  resultQuote;
+
+  constructor(
+    public getQuoteService: GetQuoteService,
+    public getAllowedCurrenciesService: GetAllowedCurrenciesService,
+    public getDestinationCountryService: GetDestinationCountryService,
+    public getHostCountryService: GetHostCountryService) { }
+
 
   ngOnInit(): void {
+    this.getHostCountry();
+    this.getDestinationCountry();
+//     this.getQuote();
   }
+
+  getAllowedCurrencies(countryValue: string){
+    this.getAllowedCurrenciesService.getAllowedCurrencies(countryValue).subscribe(currenciesResult => {
+        this.resultCurrencies = currenciesResult;
+    }, error => {
+      console.log("ERORRRRRR" + JSON.stringify(error));
+    });
+  }
+
+  getDestinationCountry(){
+    this.getDestinationCountryService.getDestinationCountry().subscribe(countriesResult => {
+        this.resultDestinationCountries = countriesResult;
+    }, error => {
+      console.log("ERORRRRRR" + JSON.stringify(error));
+    });
+  }
+
+  getHostCountry(){
+    this.getHostCountryService.getHostCountry().subscribe(countriesResult => {
+        this.resultHostCountries = countriesResult;
+    }, error => {
+      console.log("ERORRRRRR" + JSON.stringify(error));
+    });
+  }
+
+  getQuote(){
+    this.getQuoteService.getQuote().subscribe(quoteResult => {
+        this.resultQuote = quoteResult;
+        console.log("!!!! Here:   " + JSON.stringify(this.resultQuote));
+    }, error => {
+      console.log("ERORRRRRR" + JSON.stringify(error));
+    });
+  }
+
+  hostCountrySelected(){
+//     console.log(this.getQuoteForm!.get('hostCountry')!);
+    let countryValue = this.getQuoteForm.controls['hostCountry'].value;
+    this.getAllowedCurrencies(countryValue);
+    this.hostCountrySelectedBool = true;
+    const ctrl = this.getQuoteForm!.get('hostState')!;
+    const ctrl2 = this.getQuoteForm!.get('currency')!;
+    ctrl.enable();
+    ctrl2.enable();
+  }
+
 
 }
